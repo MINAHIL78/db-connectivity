08eff250cbd71081c9a276a3fb2ff2b7018bb157
MINAHIL <144820980+MINAHIL78@users.noreply.github.com>
2024-01-06 13:39:03 -0800

    Add files via upload

---
+++ hospital.php
@@ -0,0 +1,96 @@
+<!--HOSPITAL WEBSITE -->
+<?php  
+ 
+$id = "image_1";
+$src = "../skins//image_1.png";
+ 
+$style = array();
+$style[] = "top:0px";
+$style[] = "left:185px";
+$style[] = "width: 95px";
+$style[] = "height: 95px";
+$style[] = "";
+ 
+echo '<img';
+echo ' id="'.$id.'"';
+echo ' src="'.$src.'"';
+echo ' style="'.implode(";",$style).'"';
+echo ' />';
+ 
+?>
+<!--HOME-->
+<?php  
+ 
+$id = "_bg__home";
+ 
+$style = array();
+$style[] = "top:0px";
+$style[] = "left:0px";
+$style[] = "width:1366px";
+$style[] = "height:1857px";
+$style[] = "";
+ 
+echo '<div';
+echo ' id="'.$id.'"';
+echo ' style="'.implode(";",$style).'"';
+echo ' >';
+echo ' </div>';
+ 
+?>
+<!--ABOUT US-->
+<?php  
+ 
+$id = "_bg__about_us";
+ 
+$style = array();
+$style[] = "top:0px";
+$style[] = "left:0px";
+$style[] = "width:1366px";
+$style[] = "height:1857px";
+$style[] = "";
+ 
+echo '<div';
+echo ' id="'.$id.'"';
+echo ' style="'.implode(";",$style).'"';
+echo ' >';
+echo ' </div>';
+ 
+?>
+<!--DOCTOR-->
+<?php  
+ 
+$id = "_bg__doctors";
+ 
+$style = array();
+$style[] = "top:0px";
+$style[] = "left:0px";
+$style[] = "width:1366px";
+$style[] = "height:1870px";
+$style[] = "";
+ 
+echo '<div';
+echo ' id="'.$id.'"';
+echo ' style="'.implode(";",$style).'"';
+echo ' >';
+echo ' </div>';
+ 
+?>
+<!--APPOINTMENT-->
+<?php  
+ 
+$id = "_bg__appointment";
+ 
+$style = array();
+$style[] = "top:0px";
+$style[] = "left:0px";
+$style[] = "width:1366px";
+$style[] = "height:1870px";
+$style[] = "";
+ 
+echo '<div';
+echo ' id="'.$id.'"';
+echo ' style="'.implode(";",$style).'"';
+echo ' >';
+echo ' </div>';
+ 
+?>
